7e44b8c6d2bf602c313738336b1166aa4734fc74
Phlex <flexing@phlexplexi.co>
2024-06-13 14:56:36 -0600

    Add in some TODOs for what is still needed for extended object info.

--- code/include/rnd/models.h
+++ code/include/rnd/models.h
@@ -1,6 +1,7 @@
 #ifndef _RND_MODELS_H_
 #define _RND_MODELS_H_
 #include "game/actor.h"
+#include "game/actorresource.h"
 #include "rnd/item_table.h"
 #include "z3d/z3DVec.h"
 namespace rnd {
@@ -16,6 +17,10 @@ namespace rnd {
     void* saModel2;
     f32 scale;
   } Model;
+
+  s32 Object_SpawnPersistent(void* objectCtx, s16 objectId);
+  s32 Object_GetSlot(void* objectCtx, s16 objectId);
+  // TODO: IsLoaded, UpdateBank, and Clear
 
   void* SkeletonAnimationModel_Spawn(game::act::Actor*, game::GlobalContext*, s16, s32);
   void SkeletonAnimationModel_CopyMtx(void*, void*);
@@ -33,5 +38,16 @@ namespace rnd {
 
   void Model_InvertMatrixByScale(void* mtx, float scale);
 
+  // Extended Object to deal with spawning multiple actors without running out of standard game space.
+  typedef game::ActorResource::ObjectContext ExtendedObjectContext;
+  extern ExtendedObjectContext rExtentdedObjectCtx;
+
+  s32 ExtendedObject_Spawn(game::ActorResource::ObjectContext* objectCtx, s16 objectId);      // TODO: Implement
+  s32 ExtendedObject_GetIndex(game::ActorResource::ObjectContext* objectCtx, s16 objectId);   // TODO: Implement
+  game::ActorResource::ActorResource* ExtendedObject_GetStatus(s16 objectId);                 // TODO: Implement
+  s32 ExtendedObject_IsLoaded(game::ActorResource::ObjectContext* objectCtx, s16 bankIndex);  // TODO: Implement
+  void ExtendedObject_Clear(game::GlobalContext* globalCtx,
+                            game::ActorResource::ObjectContext* objectCtx);  // TODO: Implement
+  void* ExtendedObject_GetCMABByIndex(s16 objectId, u32 objectAnimIdx);      // TODO: Implement
 }  // namespace rnd
 #endif  //_RND_MODELS_H_
