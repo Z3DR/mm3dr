3ebacb1454ad76cf4e6f5f4c689b13409e48f476
Phlex <3514085+PhlexPlexico@users.noreply.github.com>
2025-06-27 13:26:11 -0600

    fix: Fix big chest and door crashes.

--- code/source/asm/fd_patches.s
+++ code/source/asm/fd_patches.s
@@ -8,6 +8,7 @@ patch_UseFDAnywhere:
 .section .patch_FDClimbingAnimationFixOne
 .global patch_FDClimbingAnimationFixOne
 patch_FDClimbingAnimationFixOne:
+  .word 0x338
   .word 0x1F9
   .word 0x1FA
   .word 0x1FB
@@ -20,3 +21,23 @@ patch_FDClimbingAnimationFixTwo:
   .word 0x1F5
   .word 0x1F5
   .word 0x1F8
+
+.section .patch_FDDoorCrashOne
+.global patch_FDDoorCrashOne
+patch_FDDoorCrashOne:
+  .word 0x2BD
+
+.section .patch_FDDoorCrashTwo
+.global patch_FDDoorCrashTwo
+patch_FDDoorCrashTwo:
+  .word 0x2BD
+
+.section .patch_FDDoorCrashThree
+.global patch_FDDoorCrashThree
+patch_FDDoorCrashThree:
+  .word 0x2BE
+
+.section .patch_FDDoorCrashFour
+.global patch_FDDoorCrashFour
+patch_FDDoorCrashFour:
+  .word 0x2BE
