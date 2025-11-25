3dc25b0cf2bc070b2144f98c4cb5aeed0cacc7c2
Phlex <3514085+PhlexPlexico@users.noreply.github.com>
2025-11-25 12:53:45 -0600

    Add in HMS ext data check.

--- code/source/asm/songsanity_hooks.s
+++ code/source/asm/songsanity_hooks.s
@@ -1,11 +1,20 @@
 .arm
 .text
 
+.global hook_EnOsnCheckSoHExtData
+hook_EnOsnCheckSoHExtData:
+    push {r0-r12, lr}
+    mov r0,#0x69
+    bl ItemOverride_ReceivedSongOverride
+    cmp r0,#0x0
+    pop {r0-r12,lr}
+    bx lr
+
 .global hook_RemoveSoSCheckKaepora
 hook_RemoveSoSCheckKaepora:
-  push {r0-r12, lr}
-  mov r0,#0x6A
-  bl ItemOverride_ReceivedSongOverride
-  cmp r0,#0x0
-  pop {r0-r12,lr}
-  bx lr
+    push {r0-r12, lr}
+    mov r0,#0x6A
+    bl ItemOverride_ReceivedSongOverride
+    cmp r0,#0x0
+    pop {r0-r12,lr}
+    bx lr
--- code/source/asm/songsanity_patches.s
+++ code/source/asm/songsanity_patches.s
@@ -1,5 +1,10 @@
 .arm
 
+.section .patch_EnOsnCheckSoHExtData
+.global patch_EnOsnCheckSoHExtData
+patch_EnOsnCheckSoHExtData:
+    bl hook_EnOsnCheckSoHExtData
+
 .section .patch_RemoveSoSCheckKaepora
 .global patch_RemoveSoSCheckKaepora
 patch_RemoveSoSCheckKaepora:
