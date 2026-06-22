f5e490a3ebb4675457403aa5d2fc09c173c9a2a7
PhlexPlexico <3514085+PhlexPlexico@users.noreply.github.com>
2026-06-22 15:00:51 -0600

    fix: Include options for new features (#141)
    * fix: Include coloured key option choice to ensure it doesn't draw coloured keys on pickup.
    
    nit: Add [[maybe_unused]] to LerpRGBA to ignore compiler warning.
    
    * fix: Include options for postman hat drawing the overridden item.

--- code/source/rnd/item_override.cpp
+++ code/source/rnd/item_override.cpp
@@ -56,8 +56,8 @@ namespace rnd {
     rItemOverrides[0].value.looksLikeItemId = 0x56;
     rItemOverrides[1].key.scene = 0x6F;
     rItemOverrides[1].key.type = ItemOverride_Type::OVR_CHEST;
-    rItemOverrides[1].value.getItemId = 0x51;
-    rItemOverrides[1].value.looksLikeItemId = 0x51;
+    rItemOverrides[1].value.getItemId = 0x76;
+    rItemOverrides[1].value.looksLikeItemId = 0x3C;
     rItemOverrides[2].key.scene = 0x12;
     rItemOverrides[2].key.type = ItemOverride_Type::OVR_COLLECTABLE;
     rItemOverrides[2].value.getItemId = 0x37;
