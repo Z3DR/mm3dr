ab2b9b17e3ae8c296c66ddaf5c81d4dbbea20ead
PhlexPlexico <3514085+PhlexPlexico@users.noreply.github.com>
2026-06-30 11:47:20 -0600

    fix: Ensure ice trap models are drawn properly. (#146)

--- code/source/rnd/models.cpp
+++ code/source/rnd/models.cpp
@@ -259,7 +259,7 @@ namespace rnd {
   void Model_LookupByOverride(Model* model, ItemOverride override) {
     if (override.key.all != 0) {
       u16 resolvedItemId = ItemOverride_SetProgressiveItemDraw(override);
-      model->itemRow = ItemTable_GetItemRow(resolvedItemId == 0 ? override.value.getItemId : resolvedItemId);
+      model->itemRow = ItemTable_GetItemRow(resolvedItemId);
     }
   }
 
