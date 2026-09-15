d1cd9db33350b3dac7e367e66e17970ef24993a0
Phlex <3514085+PhlexPlexico@users.noreply.github.com>
2026-09-15 10:51:14 -0600

    nit: Remove auto enable skipping pirates cs.

--- code/include/rnd/settings.h
+++ code/include/rnd/settings.h
@@ -463,7 +463,7 @@ namespace rnd {
     u8 skipMikauCutscene;
     u8 skipDarmaniCutscene;
     u8 skipGiantsCutscene;
-    u8 skipPiratesCs = 1;
+    u8 skipPiratesCs;
 
     // Custom Buttons
     u32 customMapButton = 0;
